0b533af0c9809adaf209cd297275c61fa767c764
Igor Perfilyev <perfilyevhead@gmail.com>
2016-03-18 09:12:03 +0000

    from push to bot

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-var schedule = require("node-schedule");
+var fs = require('fs');
+var express = require('express');
 var parse = require("./lib/parse");
-var push = require("./lib/push");
 
-var webhookUri = "https://hooks.slack.com/services/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";
+parse();
 
-schedule.scheduleJob({hour: 7, minute: 30}, function(){
-  push(webhookUri);
+var app = express();
+
+app.post('/api/message',(req, res) => {
+  var data = JSON.parse(fs.readFileSync('data/braingames-puzzles.js').toString());
+  var puzzle = data.puzzles.pop();
+  if (puzzle) {
+    data.done.push(puzzle);
+    fs.writeFileSync('data/braingames-puzzles.js', JSON.stringify(data, true, ' '));
+    res.send({"text": puzzle.name + '\r\n' + puzzle.puzzle});
+  } else res.send({});
+});
+
+var server = app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+  var addr = server.address();
+  console.log('Version: ' + process.version);
+  console.log("Slack server app listening at ", addr.address + ":" + addr.port + "; : SERVICE HOST: " + process.env.SERVICE_HOST);
 });
